15585e6a8a65db36ec9c0bf5210e8f5da35fb210
Gagan <gagandeep231020@gmail.com>
2020-05-02 15:11:18 +0530

    Made Symbol tables separate

--- classdef.h
+++ classdef.h
@@ -1,62 +1,92 @@
 #pragma once
 #include <string>
 #include <iostream>
-class Symbol
+class Symbols
+{
+    protected:
+        int lineno;
+        int colno;
+    
+};
+class SymbolId : public Symbols
 {
 public:
-    string type;
-    string val;
-    Symbol(string t, string v)
-    {
-        type = t;
-        val = v;
-    }
-    void print()
-    {
-        cout<<"Symbol type = "<<type<<"|";
-        cout<<"Symbol value="<<val<<endl;
-    }
+   string val;
+   SymbolId(string a,int lno,int cno)
+   {
+     
+       val=a;
+       lineno=lno;
+       colno=cno;
+   }
+   
 };
+class Symbolnum : public Symbols
+{
+public:
+   int val;
+   Symbolnum(string a,int lno,int cno)
+   {
+       
+       lineno=lno;
+       colno=cno;
+       val=stoi(a);             //stores the integer value represented by the string
+   }
+   
+};
+class Symbolfloat : public Symbols
+{
+    public:
+   double val;
+   Symbolfloat(string a,int lno,int cno)
+   {
+       
+       lineno=lno;
+       colno=cno;
+       val=atof(a.c_str());            //stores the decimal value represented by the string
+   }
+};
+
+
 class Token
 {
 public:
     string name;
-    int type;
+    int attr_type;
     string attr1;
     int attr2;
     Symbol *attr3;
     
-    char attr4; //new attribute for char
     
     Token(string n)
     {
         name = n;
-        type = -1; //This denotes no attribute
+        attr_type = -1; //This denotes no attribute
     }
     Token(string n, string attr)
     {
         name = n;
         attr1 = attr;
-        type = 1; //This denotes attribute of type string
+        attr_type = 1; //This denotes attribute of attr_type string
     }
     Token(string n, int attr)
     {
         name = n;
         attr2 = attr;
-        type = 2; //This denotes attribute of type int
+        attr_type = 2; //This denotes attribute of attr_type int
     }
     Token(string n, Symbol *attr)
     {
         name = n;
         attr3 = attr;
-        type = 3; //This denotes attribute of type pointer to Symbol tab;e
+        attr_type = 3; //This denotes attribute of attr_type pointer to Symbol tab;e
     }
     
     Token(string n, char attr)  //new addition of char
     {
     	name = n;
     	attr4 = attr;
-    	type = 4;
+    	attr_type = 4;
     }
     
     Token() {}
@@ -64,10 +94,10 @@ public:
     {
         std::cout << "Token name = " << name << endl;
         std::cout << "Attribute-";
-        if (type==0)cout<<"None";
-        if(type==1)cout<<attr1;
-        if(type==2)cout<<attr2;
-        if(type==3)cout<<attr3;
+        if (attr_type==0)cout<<"None";
+        if(attr_type==1)cout<<attr1;
+        if(attr_type==2)cout<<attr2;
+        if(attr_type==3)cout<<attr3;
         cout<<endl;
     }
 };
