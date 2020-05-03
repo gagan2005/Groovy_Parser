5f169b3be95a859329ab2de83cb60ddc5af1615d
Gagan <gagandeep231020@gmail.com>
2020-05-03 14:08:23 +0530

    Added test file

---
+++ test.groovy
@@ -0,0 +1,24 @@
+def addd(a,b)
+{
+ return (a+b)
+}
+int x=2
+int y=3
+z=addd(x,y)
+print z
+
+def n=100     //this is a sample comment
+def m=2          /* A multiline 
+comment */
+def i=0
+for(i=0;i<n;i++)
+{
+    println (i*b);
+}
+switch(b) {
+    case 1:println(1)
+        
+    break
+    case 2:println(2)
+    break
+}
