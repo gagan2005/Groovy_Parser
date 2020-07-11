2578e9bc8db3d2f7d7de448dcfa9e6e2d1682845
swapnilnarad2000 <57103911+swapnilnarad2000@users.noreply.github.com>
2020-07-12 01:10:21 +0530

    Add files via upload

---
+++ p4.groovy
@@ -0,0 +1,16 @@
+float count2 = 2.4;
+//do while loop in groovy
+
+do {
+	count2 -= 0.4;
+//switch case in groovy
+	switch (count2){
+		case 2:
+			println "its 2";
+		case 1.6:
+			println "its 1.6";
+		default:
+			println "less than 1.5";
+    }
+	}while(count2 >= 0.4);
+  
