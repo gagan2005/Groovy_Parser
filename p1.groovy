3b6cda46e80de0048b9cbf32b1ebfa8d403e6a9b
swapnilnarad2000 <57103911+swapnilnarad2000@users.noreply.github.com>
2020-07-12 00:54:37 +0530

    Update p1.groovy

--- p1.groovy
+++ p1.groovy
@@ -4,9 +4,9 @@ there are many statements contained in groovy some examples are below
 
 // comment in groovy
 import groovy.xml.MarkupBuilder
-import java.lang.*
-import static term1.term2
-import static Calendar.getInstance as human;
+import groovy.xml.StreamingMarkupBuilder
+import groovy.xml.*
+import static Calendar.getInstance as now;
 
 //function declaration in groovy
 def function(int a ,int b)
@@ -29,22 +29,6 @@ while(count<5) {
     count ++;
     }
 
-float count2 = 2.4;
-//do while loop in groovy
-do {
-	count2 -= 0.4;
-//switch case in groovy
-	switch (count2){
-		case 2:
-			println "its 2";
-		case 1.6:
-			println "its 1.6";
-		default:
-			println "less than 1.5";
-    }
-	}while(count2 >= 0.4);
-  
-
 int[] array = [1,2,3,4]; 
 //for in loop in groovy		
     for( i in array) { 
@@ -55,22 +39,3 @@ int[] array = [1,2,3,4];
          println(i);
       }
 
-int[] intArray = [200, 300, 100];
-println intArray;
-//function call in groovy
-sort(intArray)  ;    
-println intArray;
-
-for(item in arr){
-//if else statement in groovy
-  def nextItem = givenNo - item ;// substract each item from given no
-  if(contains){
-     set1 = item  ;
-  }
-  else{
-     def temp = [item,nextItem]; // temp list to get max and min
-     def m1 = temp ;// max of two items
-     def m2 = temp ;// min of two items
-      set2 = [m1,m2] ;// push list(pair) inside a set
-  }  
- }
